aec8bd08b7ea3c13025a4a5f52a7f91ef816d3c8
hidems <miura.eleven@gmail.com>
2021-05-07 23:02:45 +0200

    Add header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <myheader></myheader>
+    <p v-if="msg.length > 0">
+      {{ msg }}
+    </p>
+    <p v-else>
+      No Text
+    </p>
+    <input type="text" v-model="msg">
+    <button @click="clear()">clear</button>
+  </div>
+
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// export default {
+//   name: 'App',
+//   components: {
+//     HelloWorld
+//   }
+// }
+import myheader from './components/myheader'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    myheader
+  },
+  data () {
+    return {
+      msg: 'Hello World'
+    }
+  },
+  methods: {
+    clear () {
+      this.msg = ''
+    }
+  },
   }
-}
 </script>
 
 <style>
