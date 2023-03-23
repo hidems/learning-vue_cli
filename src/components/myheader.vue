588e65d7c4a21865b196e53657fb4756eb9ee1d0
hidems <miura.eleven@gmail.com>
2023-03-24 00:21:51 +0100

    scss test

--- src/components/myheader.vue
+++ src/components/myheader.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
-    This is Header.
+    <div class="border__blue">
+      This is Header.
+    </div>
   </div>
 </template>
+
+<style scoped lang="scss">
+.border {
+  &__blue {
+    border: 1px solid blue;
+  }
+}
+</style>
